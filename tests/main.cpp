8dddfa6e7e38abdb09964c8d0dbef4aab3f644a0
Barry <barry.w.robinson64@gmail.com>
2021-09-11 09:05:08 +0100

    Removing debug output

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,23 +4,25 @@
  * @brief Test harness for filescan application library
  * @version 0.1
  * @date 2021-08-31
- * 
+ *
  * @copyright Copyright (c) 2021
- * @addtogroup test 
+ * @addtogroup test
  * @{
  * @addtogroup main
  * @{
- * 
+ *
  */
 
 
 #include <gtest/gtest.h>
+#include "libfilestat.h"
+
 /**
  * @brief Main function for test harness
- * 
- * @param argc 
- * @param argv 
- * @return int 
+ *
+ * @param argc
+ * @param argv
+ * @return int
  */
 int main(int argc, const char ** argv) {
     testing::InitGoogleTest(&argc, (char**) argv);
